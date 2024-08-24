2e10bc11971bf3f86f1e851bbf7a990954cdad5d
Pxndxs 🐼 <pxnditxyr@gmail.com>
2024-08-23 23:54:31 -0400

    refactor: restructure project to screaming architecture, move 'title' component to shared modules 📐🔧🏗️ feat: create constants for site info to standardize use across the project 📝🔧🌐 feat: add new main layout to be used across all documents 🛠️📃🔧

---
+++ src/constants/site-info.ts
@@ -0,0 +1,6 @@
+
+export const siteInfo = {
+  title: '🚨 Alertas de Desastres Naturales 🌊',
+  description: '🌍 AI-powered system for predicting and mitigating natural disasters. 🌪️ Keeping communities safe with cutting-edge technology. 🛡️',
+  author: 'Pxndxs 🐼'
+}
