6178fc8ed4f9c8252545f9f270789d0b96c5ec4f
Pxndxs 🐼 <pxnditxyr@gmail.com>
2024-08-26 05:24:16 -0400

    feat: replace Solid JS with React for dynamic functionality 🔄🔧 feat: integrate Leaflet for map display on the main screen 🗺️✨ feat: add a simple footer with animation 🎨👣 feat: include social media links in the footer 🌐🔗 style: modify CardInfo design to standardize height 🎨📏 feat: add new SVG icons 🎨🆕

--- src/modules/home/components/maps/HomeMap.tsx
+++ src/modules/home/components/maps/HomeMap.tsx
@@ -1,15 +1,37 @@
-import { createSignal } from 'solid-js'
+import { MapContainer, Marker, Polygon, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+//import { statesData } from './data'
 
 export const HomeMap = () => {
 
-  const [ counter, setCounter ] = createSignal( 10 )
+  const center  = {
+    lat: -16.49566907988408,
+    lng: -68.13395414034433
+  }
+
+  const zoom = 13
+  //const url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+  const url = "https://api.maptiler.com/maps/streets-v2-dark/256/{z}/{x}/{y}.png?key=iWkvsZBhRLHolBePP8yt"
+  const attribution = '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
 
   return (
-    <div>
-      <h1>HomeMap</h1>
-      <button onClick={ () => setCounter( counter() + 1 ) }>Increment</button>
-      <button onClick={ () => setCounter( counter() - 1 ) }>Decrement</button>
-      <p>{ counter() }</p>
+    <div className="flex items-center justify-center rounded-xl">
+      <MapContainer
+        center={ center }
+        zoom={ zoom }
+        scrollWheelZoom={ false }
+        style={{
+          width: '400px',
+          height: '50vh',
+          borderRadius: '10px',
+        }}
+      >
+        <TileLayer
+          url={ url }
+          attribution={ attribution }
+        />
+      </MapContainer>
     </div>
   )
 }
+
