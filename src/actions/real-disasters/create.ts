0582b9043c140b6858d310006dbf61a47ca3aad2
Pxndxs 🐼 <pxnditxyr@gmail.com>
2024-11-17 17:36:07 -0400

    feat(real-disasters-crud): complete full CRUD functionality for real disasters management 🌪️🛠️📋

--- src/actions/real-disasters/create.ts
+++ src/actions/real-disasters/create.ts
@@ -5,20 +5,20 @@ import { z } from 'astro:schema'
 export const createRealDisaster = defineAction({
   accept: 'form',
   input: z.object({
+    description: z.string({ message: '📝 La descripción debe tener al menos 2 caracteres.' }),
     date: z.string({ message: '📅 La fecha debe ser válida.' }),
     address: z.string({ message: '🏠 La dirección debe tener al menos 2 caracteres.' }),
     disasterTypeId: z.number({ message: '🌪️ El tipo de desastre debe ser válido.' }),
     severityLevel: z.number({ message: '📈 El nivel de severidad debe ser válido.' }),
-    description: z.string({ message: '📝 La descripción debe tener al menos 2 caracteres.' }),
     imageUrl: z.string({ message: '📸 La imagen debe ser una URL válida.' }).optional(),
   }),
   handler: async ( { date, address, disasterTypeId, severityLevel, description, imageUrl } ) => {
     await db.insert( RealDisaster ).values({
+      description,
       date: new Date( date ),
       address,
       disasterTypeId,
       severityLevel,
-      description,
       imageUrl,
     })
 
